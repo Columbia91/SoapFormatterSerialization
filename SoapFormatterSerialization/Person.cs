1588886c6b104ea77d18396f244b30ba921dc16d
cougar91 <alpha.z91@mail.ru>
2019-03-10 12:49:02 +0600

    Добавьте файлы проекта.

---
+++ SoapFormatterSerialization/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoapFormatterSerialization
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public int YearOfBirth { get; set; }
+
+        public Person(string name, string surname, string number, int year)
+        {
+            Name = name; Surname = surname; PhoneNumber = number; YearOfBirth = year;
+        }
+    }
+}
---
+++ SoapFormatterSerialization/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SoapFormatterSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Брэдли", "Купер", "+77011012233", 1980);
+            Person person2 = new Person("Эбби", "Корниш", "+77014040140", 1985);
+            Person[] people = { person, person2 };
+
+            // Задание: Сериализовать коллекцию с помощью класса SoapFormatter 
+            // SoapFormatter не поддерживает сериализацию коллекции https://john475.files.wordpress.com/2014/07/serializecomparison.png
+            SoapFormatter soapFormatter = new SoapFormatter();
+            string path = "people.csv";
+            using (FileStream fs = File.Create(path))
+            {
+                soapFormatter.Serialize(fs, people);
+            }
+
+            List<Person> list = new List<Person>();
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                Person[] newPeople = (Person[])soapFormatter.Deserialize(fs);
+                for (int i = 0; i < newPeople.Length; i++)
+                {
+                    list.Add(newPeople[i]);
+                }
+            }
+
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            using(StreamWriter fs = new StreamWriter("people.json"))
+            {
+                jsonSerializer.Serialize(fs, list);
+            }
+        }
+    }
+}
